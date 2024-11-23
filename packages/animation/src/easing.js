023dfc404fce1947589d13fc541645b961d8ec41
dmail <dmaillard06@gmail.com>
2024-11-23 18:43:02 +0100

    work on easing

--- packages/animation/src/easing.js
+++ packages/animation/src/easing.js
@@ -5,6 +5,9 @@ export const EASING = {
   EASE: (x) => {
     return bezier(x, 0.25, 0.1, 0.25, 1.0);
   },
+  EASE_IN_OUT_CUBIC: (x) => {
+    return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
+  },
   EASE_OUT_EXPO: (x) => {
     return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
   },
