931f20988b98ee101b6d71a2042f9a75fea46a4d
dmail <dmaillard06@gmail.com>
2024-12-16 14:43:49 +0100

    working

--- src/app/canvas/draw.js
+++ src/app/canvas/draw.js
@@ -1,38 +1,20 @@
 let displayDrawPoints = false;
 
-export const drawPoint = (context, [x, y], { color } = {}) => {
-  context.beginPath();
+export const drawPoint = (context, [x, y]) => {
   context.arc(x, y, 2, 0, Math.PI * 2);
-  if (color) {
-    context.fillStyle = color;
-    context.fill();
-  }
 };
 
-export const drawLine = (context, start, end, { color } = {}) => {
-  context.beginPath();
+export const drawLine = (context, start, end) => {
   context.moveTo(start[0], start[1]);
   context.lineTo(end[0], end[1]);
-  if (color) {
-    context.strokeStyle = color;
-    context.stroke();
-  }
   if (displayDrawPoints) {
     drawPoint(context, start, { color: "aqua" });
     drawPoint(context, end, { color: "chocolate" });
   }
 };
 
-export const drawArc = (
-  context,
-  center,
-  radius,
-  fromDegrees,
-  toDegrees,
-  { color } = {},
-) => {
+export const drawArc = (context, center, radius, fromDegrees, toDegrees) => {
   const [centerX, centerY] = center;
-  context.beginPath();
   context.arc(
     centerX,
     centerY,
@@ -40,10 +22,6 @@ export const drawArc = (
     radianFromDegree(fromDegrees),
     radianFromDegree(toDegrees),
   );
-  if (color) {
-    context.strokeStyle = color;
-    context.stroke();
-  }
   if (displayDrawPoints) {
     drawPoint(context, center, { color: "chartreuse" });
   }
@@ -52,27 +30,33 @@ export const radianFromDegree = (degrees) => {
   return degrees * (Math.PI / 180);
 };
 
-export const drawArcTo = (
-  context,
-  start,
-  controlPoint,
-  end,
-  radius,
-  { color } = {},
-) => {
+export const drawArcTo = (context, start, controlPoint, end, radius) => {
   if (radius < 0) {
     return;
   }
-  context.beginPath();
   context.moveTo(start[0], start[1]);
   context.arcTo(controlPoint[0], controlPoint[1], end[0], end[1], radius);
-  if (color) {
-    context.strokeStyle = color;
-    context.stroke();
-  }
   if (displayDrawPoints) {
     drawPoint(context, start, { color: "chartreuse" });
     drawPoint(context, controlPoint, { color: "violet" });
     drawPoint(context, end, { color: "chartreuse" });
+  }
+};
+
+export const drawQuadraticCurveTo = (
+  context,
+  start,
+  controlPoint,
+  endPoint,
+) => {
+  let [startX, startY] = start;
+  let [controlX, controlY] = controlPoint;
+  let [endX, endY] = endPoint;
+  context.moveTo(startX, startY);
+  context.quadraticCurveTo(controlX, controlY, endX, endY);
+  if (displayDrawPoints) {
+    drawPoint(context, start, { color: "chartreuse" });
+    drawPoint(context, controlPoint, { color: "violet" });
+    drawPoint(context, endPoint, { color: "chartreuse" });
   }
 };
